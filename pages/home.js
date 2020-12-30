7b19f813eb3bc0d1c6e64618873dfce40de2755f
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-30 19:43:22 +0100

    change home name and refactor

---
+++ pages/home.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import { Container } from '../components/Container';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <Container>
+      <p>kjbjbjb</p>
+    </Container>
+  );
+}
