c007e6f78d19fc6af0bbeb4307badc1da512373a
MPiotrowska <monikapiotrowska27@gmail.com>
2020-12-30 20:00:48 +0100

    add text

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import { Container } from '../components/Container';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <Container></Container>;
+  return <Container>Home</Container>;
 }
